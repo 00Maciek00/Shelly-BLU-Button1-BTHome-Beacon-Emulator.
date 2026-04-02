81b276a550e7929b56ee16e6e89590c9bc7ea194
00Maciek00 <maciej.sikorski01@gmail.com>
2026-04-02 18:43:08 +0200

    Update beacon.ino

--- beacon.ino
+++ beacon.ino
@@ -45,7 +45,7 @@
  *   Apache License 2.0
  *   https://www.apache.org/licenses/LICENSE-2.0
  *
-  * Copyright (c) 2026 Maciej Sikorski (S.M. DIY Home)
+ * Copyright (c) 2026 Maciej Sikorski (S.M. DIY Home)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
